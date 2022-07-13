f682ee26ecee0baf0c60c321e22503568a00bb47
Bhavesh702 <bhaveshmandlik1@gmail.com>
2022-07-13 11:19:28 +0530

    This is second modification

--- src/LogicalProgramme/Swap2Numbers.java
+++ src/LogicalProgramme/Swap2Numbers.java
@@ -3,7 +3,8 @@ package LogicalProgramme;
 public class Swap2Numbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Hii Mamu log
+		//This is the changes.
 
 	}
 
